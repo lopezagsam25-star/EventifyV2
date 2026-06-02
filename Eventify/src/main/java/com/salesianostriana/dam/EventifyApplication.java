b4fdbe21b1faaa1b913e1ce0c014527a26f83fd0
lopezagsam25-star <lopez.agsam25@triana.salesianos.edu>
2026-06-02 13:17:23 +0200

    v1

---
+++ Eventify/src/main/java/com/salesianostriana/dam/EventifyApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventifyApplication.class, args);
+	}
+
+}
